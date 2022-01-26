999479a8cc63563d58fa39416af81dd29388658b
yusei-hyodo <yusei.hyodo@youlib.co.jp>
2022-01-26 19:07:06 +0900

    amplify init

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "nextamplifyliff": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
